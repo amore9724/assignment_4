f64ed8b1f53f60b675b6242d19cccb9da460dd1a
amore9724 <arya.more.edu4@gmail.com>
2026-04-04 20:38:13 -0700

    Initial commit

---
+++ A3.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef _A3_H_
+#define _A3_H_
+#define MSIZE 10001 // Maximum number of lines in the program(empty or non empty)
+#define MNAME 101   // Maximum number of names.
+#define MLINE 31    // Maximum number of characters in a line.
+#define MAXLINE 4096    // This is the maximum amount of lines that will be read.
+#define PROGRAMNAME "countnames"    // This is the name of the program to be executed.
+#define MAXARGS 100  // Maximum number of arguments
+
+typedef struct {
+    char name[31];
+    int count;
+} NameCountData;    // Contains the name and the amount of time it appears in a file.
+
+typedef enum {
+    TYPE_NAMECOUNT,
+    TYPE_B          // This enum may be extended with other types in the future.
+} MessageType;
+
+typedef struct {
+    MessageType type;
+    size_t size;    // Size of the following payload
+} MessageHeader;    // Message to be sent through pipes.
+
+int check_in(char *a, char *b[]); // Checks if a string is in the file.
+void nprinter(char *nused[], int count[]); // Prints a string and the amount of times it occurs.
+void clnup(char *a1[], char *a2[]); // Frees allocated memory.
+void ncount(char *arr[], char *nused[], int count[]); // Counts the number of times a string occurs in a file.
+#endif
---
+++ countnames.c
@@ -0,0 +1,89 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include "A3.h"
+
+
+/*
+ *   NOTE: A history of this code is available on a GitHub repository.
+ *   This repository can be made available upon request.
+ */
+
+void write_struct_namecount(int fd, NameCountData *data) {
+    MessageHeader header;
+    header.type = TYPE_NAMECOUNT;
+    header.size = sizeof(NameCountData);
+
+    write(fd, &header, sizeof(header)); // Write the header
+    write(fd, data, sizeof(NameCountData)); // Write the payload (name counts)
+}
+
+int main(int argc, char *argv[]) /* int argc = argument count
+                                  * char *argv[] = string array containing the actual arguments passed.*/
+
+{
+    char filename[64];
+    sprintf(filename, "output/%d.out", getpid());
+
+    /* Create a PID.out for this child process
+    and then set stdout to this PID.out */
+
+    if (freopen(filename, "w", stdout) == NULL) {
+        perror("freopen failed");
+        return 1;
+    }
+
+    /* Create a PID.err for this child process
+    and then set stderr to this PID.err */
+
+    char fileerr[64];
+    sprintf(fileerr, "output/%d.err", getpid());
+
+    if (freopen(fileerr, "w", stderr) == NULL) {
+        perror("freopen failed");
+        return 1;
+    }
+    if (argc == 1) // If no file was provided
+    {
+        puts("No file provided, exiting."); // This informs the user that there is no file.
+        return 0;
+    }
+
+    FILE *f = fopen(argv[1], "r");      // Opens file to read from.
+    if (f == NULL)
+    {
+        fprintf(stderr, "Error: Child %d could not open file %s and is exiting.\n", getpid(), argv[1]);
+        return 1;
+    }
+
+    int i = 0, lnum = 0;
+    char namebuf[MLINE] = {0}; // This buffer temporarily stores a line in the file.
+    char *names[MSIZE] = {0};  // This stores all the names and their occurences in the file.
+    while (fgets(namebuf, MLINE, f))
+    {
+        lnum++;
+        char *tok = strtok(namebuf, "\n"); // Uses "\n" character to tokenize string.
+        if (tok == NULL)
+        {
+            fprintf(stderr, "Warning: Line %d in Child %d is empty.\n", lnum, getpid());
+            continue;
+        }
+        names[i++] = strdup(tok); /* This allocates memory on the heap to store the string,
+                                      which needs to be freed later. */
+    }
+
+    fclose(f);
+
+    int count[MNAME] = {0};   // Contains the number of times a name occurs in the file.
+    char *nused[MNAME] = {0}; // Contains the number of unique names used in the file.
+    ncount(names, nused, count);    // Counts the names used and sends it to the arrays.
+    for (i = 0; nused[i] != 0; i++) { // Sends data to parent
+        NameCountData ncd;                 // Initializes NameCountData struct to send to parent.
+        strcpy(ncd.name, nused[i]);         // Copies the current name into the communication header.
+        ncd.count = count[i];               // Sets the count of the current name to be set.
+        write_struct_namecount(STDOUT_FILENO, &ncd);    // Sends the name count to parent.
+    }
+
+    fflush(stdout);     // Flushes stdout to prevent further issues.
+    clnup(names, nused); // This will free the allocated memory.
+    return 0;
+}
